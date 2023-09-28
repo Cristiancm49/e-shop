55e03960ea3ad4b5ff4ca9f7c400cf910e148ca2
Cristiancm49 <ccortesm2000@gmail.com>
2023-09-28 12:29:31 -0500

    Estilos a la vista de las ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,13 +1,22 @@
-import { XMarkIcon } from "@heroicons/react/24/solid"
+import { CurrencyDollarIcon, ShoppingBagIcon, CalendarDaysIcon } from "@heroicons/react/24/solid"
 
 const OrdersCard = props => {
-    const { totalPrice, totalProducts } = props
+    const { totalPrice, totalProducts  } = props
     return (
-        <div className='flex justify-between w-full my-2 border border-white'>
-            <p>
-                <span>01.02.23</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
+        <div className='flex justify-between w-full h-20 my-2 border rounded-lg border-white items-center px-4'>
+            <p className='flex justify-between w-80 '>
+                <span className='flex items-center'>
+                    <CalendarDaysIcon className='w-5 h-5 mx-3' />
+                    01-02-23
+                </span>
+                
+                <span className='flex items-center'>
+                    <ShoppingBagIcon className='w-5 h-5 mx-1'/>
+                    {totalProducts} Products
+                </span>
+                <span className='flex items-center'>
+                    <CurrencyDollarIcon className='w-5 h-5 mx-1'/>
+                    $ {totalPrice}</span>
             </p>
         </div>
     )
