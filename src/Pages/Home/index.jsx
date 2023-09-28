bf036442ad89d33fae8317c40ba2970391e11429
Cristiancm49 <ccortesm2000@gmail.com>
2023-09-28 13:33:55 -0500

    refactor del codigo, ajute del context

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card/Card'
 import ProductDetail from '../../Components/ProductDetail'
+import { ShoppingCartContext } from '../../Context'
 
 
 function Home () {
 
-
+    const context = useContext(ShoppingCartContext)
     return (
         <Layout>
             <div className='grid gap-4 px-5 grid-cols-2 w-full max-w-screen-lg lg:grid-cols-4'>
                 {
-                    items?.map(item => (<Card key={item.id} data={item} />))
+                    context.items?.map(item => (<Card key={item.id} data={item} />))
                 }
             </div>
             <ProductDetail />
